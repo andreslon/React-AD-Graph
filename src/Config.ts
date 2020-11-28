9233a4d2eb8b019d24bd46cfa38ff30bf8680ea7
Andrés Londoño <andres.londono@teaminternational.com>
2020-11-27 23:06:20 -0500

    example project with AD and graph implementation

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  appId: 'fa172418-8884-44f9-b368-953bd823c685',
+  redirectUri: 'http://localhost:3000',
+  scopes: [
+    'user.read',
+    'mailboxsettings.read',
+    'calendars.readwrite'
+  ]
+};
